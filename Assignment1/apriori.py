a4ec33669702c4a8cedbcb719315c15b6b9d6246
Yune Cho <fusma159@naver.com>
2025-04-02 00:42:27 +0900

    파일 입출력 구현

--- Assignment1/apriori.py
+++ Assignment1/apriori.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import sys
+
+args = sys.argv[1:]
+minSupport = args[0]
+input = open(args[1], 'r')
+output = open(args[2], 'w')
+
+while True:
+    line = input.readline()
+    if not line: break
+    print(line)
+
+input.close()
+output.close()
