65d99c3cd2fcd5fe8046b644f291396ed17630d6
Yune Cho <fusma159@naver.com>
2025-04-29 11:15:06 +0900

    main 함수 추가

--- Assignment2/decision_tree.py
+++ Assignment2/decision_tree.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+# Environment
+# OS: MacOS
+# Language: Python 3.13
+
+import sys
+
+def main():
+    args = sys.argv[1:]
+    with open(args[0], 'r') as train:
+        # 훈련 데이터 읽어오는 함수 추가
+        train.close()
+    # 훈련 수행 코드 구현
+    with open(args[1], 'r') as test:
+        # 테스트 데이터 읽어오는 함수 추가
+        test.close()
+    # 테스트 수행 코드 구현
+    with open(args[2], 'w') as result:
+        # 결과 작성하는 함수 추가
+        result.close()
+
+if __name__ == '__main__':
+    main()
