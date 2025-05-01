9660ca3115d1a92411137e253c52214861360ea7
Yune Cho <fusma159@naver.com>
2025-05-02 00:18:29 +0900

    파일 입출력 구현

--- Assignment2/decision_tree.py
+++ Assignment2/decision_tree.py
@@ -4,18 +4,31 @@
 
 import sys
 
+def get_data(file):
+    lines = file.readlines()
+    attribute_names = lines[0].strip().split('\t')
+    attribute_values = []
+    for line in lines[1:]:
+        attribute_values.append(line.strip().split('\t'))
+    return attribute_names, attribute_values
+
+def write_result(file, attributes, data):
+    file.write('\t'.join(attributes) + '\n')
+    for line in data:
+        file.write('\t'.join(line) + '\n')
+
 def main():
     args = sys.argv[1:]
     with open(args[0], 'r') as train:
-        # 훈련 데이터 읽어오는 함수 추가
+        train_attribute_names, train_data = get_data(train)
         train.close()
     # 훈련 수행 코드 구현
     with open(args[1], 'r') as test:
-        # 테스트 데이터 읽어오는 함수 추가
+        test_attribute_names, test_data = get_data(test)
         test.close()
     # 테스트 수행 코드 구현
     with open(args[2], 'w') as result:
-        # 결과 작성하는 함수 추가
+        write_result(result, train_attribute_names, train_data)
         result.close()
 
 if __name__ == '__main__':
