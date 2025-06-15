c4fab12b6b4cd0430bc8361a4e2ec368eea539a2
Yune Cho <fusma159@naver.com>
2025-06-16 00:29:33 +0900

    코드 수정

--- Assignment3/dbscan.py
+++ Assignment3/dbscan.py
@@ -12,32 +12,13 @@ def get_data(file):
         data.append((int(tmp[0]), float(tmp[1]), float(tmp[2])))
     return data
 
-def calculate_distance(point1, point2):
-    distance = math.sqrt((point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
-    return distance
-
 def range_query(data, point_index, eps):
     neighbors = []
     for i in range(len(data)):
-        distance = calculate_distance(data[point_index], data[i])
+        distance = math.sqrt((data[point_index][1] - data[i][1])**2 + (data[point_index][2] - data[i][2])**2)
         if distance < eps:
             neighbors.append(i)
     return neighbors
-
-def expand_neighborhood(data, label, neighbors, cluster_id, eps, min_pts):
-    i = 0
-    while i < len(neighbors):
-        neighbor_index = neighbors[i]
-        if label[neighbor_index] == -1:
-            label[neighbor_index] = cluster_id
-        elif label[neighbor_index] == 0:
-            new_neighbors = range_query(data, neighbor_index, eps)
-            label[neighbor_index] = cluster_id
-            if len(new_neighbors) >= min_pts:
-                for new_neighbor in new_neighbors:
-                    if new_neighbor not in neighbors:
-                        neighbors.append(new_neighbor)
-        i += 1
 
 def dbscan(data, eps, min_pts):
     label = [0] * len(data)
@@ -50,19 +31,32 @@ def dbscan(data, eps, min_pts):
             else:
                 cluster_id += 1
                 label[point_index] = cluster_id
-                expand_neighborhood(data, label, neighbors, cluster_id, eps, min_pts)
+                i = 0
+                while i < len(neighbors):
+                    if label[neighbors[i]] == -1:
+                        label[neighbors[i]] = cluster_id
+                    elif label[neighbors[i]] == 0:
+                        new_neighbors = range_query(data, neighbors[i], eps)
+                        label[neighbors[i]] = cluster_id
+                        if len(new_neighbors) >= min_pts:
+                            for new_neighbor in new_neighbors:
+                                if new_neighbor not in neighbors:
+                                    neighbors.append(new_neighbor)
+                    i += 1
     return label
 
 def write_results(input_filename, clusters, n):
-    filename = input_filename.split('.')[0]
+    filename = input_filename.removesuffix(".txt")
     sorted_clusters = sorted(list(clusters.values()), key=len, reverse=True)
-    top_n_clusters = sorted_clusters[:n]
-    for i, cluster in enumerate(top_n_clusters):
+    n_clusters = sorted_clusters[:n]
+    i = 0
+    for cluster in n_clusters:
         output_filename = f"{filename}_cluster_{i}.txt"
         with open(output_filename, 'w') as output:
             for object_id in cluster:
                 output.write(f"{object_id}\n")
             output.close()
+        i += 1
 
 def main():
     args = sys.argv[1:]
@@ -76,7 +70,8 @@ def main():
     min_pts = int(args[3])
     labels = dbscan(input_data, eps, min_pts)
     clusters = {}
-    for i, label in enumerate(labels):
+    for i in range(len(labels)):
+        label = labels[i]
         if label > 0:
             if label not in clusters:
                 clusters[label] = []
